ac92562de3fbff0410c9c7781a301ba11231de7c
Waslah <151165442+Waslah@users.noreply.github.com>
2024-07-31 13:35:26 -0700

    Tailwind Project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#000000',
+        'anuj': '#01ee91'
+      }
+    },
+  },
+  plugins: [],
+}
+
